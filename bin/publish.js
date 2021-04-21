7989182a117324d71ba3ac0638db4b0bd2c961d8
stpham <stpham@linkedin.com>
2021-04-20 18:10:15 -0700

    revert publish split

--- bin/publish.js
+++ bin/publish.js
@@ -1,18 +1,48 @@
+#!/usr/bin/env node
+
 'use strict';
 /* eslint-disable node/no-unsupported-features/es-syntax */
 
+/*
+Usage
+
+publish lts|release|beta|canary
+
+Flags
+
+--distTag=latest|lts|beta|canary|release-<major>-<minor>
+--bumpMajor
+--bumpMinor
+--skipVersion
+--skipPack
+--skipPublish
+--skipSmokeTest
+
+Inspiration from https://github.com/glimmerjs/glimmer-vm/commit/01e68d7dddf28ac3200f183bffb7d520a3c71249#diff-19fef6f3236e72e3b5af7c884eef67a0
+*/
+
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 const process = require('process');
 
-const execa = require('execa');
 const chalk = require('chalk');
-const debug = require('debug')('publish-packages');
+const execa = require('execa');
+const cliArgs = require('command-line-args');
 const semver = require('semver');
+const debug = require('debug')('publish-packages');
 
-const projectRoot = path.resolve(__dirname, '../../');
+const projectRoot = path.resolve(__dirname, '../');
 const packagesDir = path.join(projectRoot, './packages');
 const packages = fs.readdirSync(packagesDir);
+const PreviousReleasePattern = /^release-(\d)-(\d+)$/;
+
+let isBugfixRelease = false;
+
+function cleanProject() {
+  execWithLog(`cd ${projectRoot} && rm -rf packages/*/dist packages/*/tmp packages/*/node_modules node_modules`);
+  execWithLog(`cd ${projectRoot} && yarn install`);
+}
 
 /**
  *
@@ -30,9 +60,57 @@ function execWithLog(command, proxyIO = false) {
   return execa.sync(command, { shell: true, preferLocal: true }).stdout;
 }
 
-function cleanProject() {
-  execWithLog(`cd ${projectRoot} && rm -rf packages/*/dist packages/*/tmp packages/*/node_modules node_modules`);
-  execWithLog(`cd ${projectRoot} && yarn install`);
+function getConfig() {
+  const mainOptionsDefinitions = [{ name: 'channel', defaultOption: true }];
+  const mainOptions = cliArgs(mainOptionsDefinitions, { stopAtFirstUnknown: true });
+  const argv = mainOptions._unknown || [];
+
+  if (!mainOptions.channel) {
+    throw new Error(`Incorrect usage of publish:\n\tpublish <channel>\n\nNo channel was specified`);
+  }
+  if (!['release', 'beta', 'canary', 'lts'].includes(mainOptions.channel)) {
+    const channel = mainOptions.channel;
+    let potentialRelease = !!channel && channel.match(PreviousReleasePattern);
+    if (potentialRelease && Array.isArray(potentialRelease)) {
+      isBugfixRelease = true;
+    } else {
+      throw new Error(
+        `Incorrect usage of publish:\n\tpublish <channel>\n\nChannel must be one of release|beta|canary|lts. Received ${mainOptions.channel}`
+      );
+    }
+  }
+
+  const optionsDefinitions = [
+    {
+      name: 'distTag',
+      alias: 't',
+      type: String,
+      defaultValue: mainOptions.channel === 'release' ? 'latest' : mainOptions.channel,
+    },
+    { name: 'skipVersion', type: Boolean, defaultValue: false },
+    { name: 'skipPack', type: Boolean, defaultValue: false },
+    { name: 'skipPublish', type: Boolean, defaultValue: false },
+    { name: 'skipSmokeTest', type: Boolean, defaultValue: false },
+    { name: 'bumpMajor', type: Boolean, defaultValue: false },
+    { name: 'bumpMinor', type: Boolean, defaultValue: false },
+    { name: 'force', type: Boolean, defaultValue: false },
+    { name: 'useVersion', type: String, defaultValue: false },
+  ];
+  const options = cliArgs(optionsDefinitions, { argv });
+  const currentProjectVersion = require(path.join(__dirname, '../lerna.json')).version;
+
+  if (isBugfixRelease && (options.bumpMajor || options.bumpMinor)) {
+    throw new Error(`Cannot bump major or minor version of a past release`);
+  }
+
+  if (options.bumpMinor && options.bumpMajor) {
+    throw new Error(`Cannot bump both major and minor versions simultaneously`);
+  }
+
+  options.channel = mainOptions.channel;
+  options.currentVersion = currentProjectVersion;
+
+  return options;
 }
 
 function assertGitIsClean(options) {
@@ -115,7 +193,7 @@ function assertGitIsClean(options) {
   }
 }
 
-function retrieveNextVersion(options, isBugfixRelease) {
+function retrieveNextVersion(options) {
   /*
 
   A brief rundown of how version updates flow through the branches.
@@ -215,6 +293,17 @@ function packAllPackages() {
   });
 }
 
+async function getOTPToken() {
+  let token = await question(chalk.green('\nPlease provide OTP token '));
+
+  return token.trim();
+}
+function question(prompt) {
+  return new Promise(resolve => {
+    cli.question(prompt, resolve);
+  });
+}
+
 /**
  * If otp is passed add it as a parameter to the publish command else assume authentication is setup either
  * as environment variable
@@ -233,12 +322,98 @@ function publishPackage(distTag, tarball, otp) {
   execWithLog(cmd);
 }
 
-module.exports = {
-  assertGitIsClean,
-  cleanProject,
-  retrieveNextVersion,
-  execWithLog,
-  packAllPackages,
-  collectTarballPaths,
-  publishPackage,
-};
+async function confirmPublish(tarballs, options, promptOtp = true) {
+  let otp;
+
+  if (promptOtp) {
+    otp = await getOTPToken();
+  }
+
+  for (let tarball of tarballs) {
+    try {
+      publishPackage(options.distTag, tarball, otp);
+    } catch (e) {
+      // the token is outdated, we need another one
+      if (e.message.includes('E401') || e.message.includes('EOTP')) {
+        otp = await getOTPToken();
+
+        publishPackage(options.distTag, tarball, otp);
+      } else {
+        throw e;
+      }
+    }
+  }
+}
+
+async function main() {
+  const options = getConfig();
+
+  assertGitIsClean(options);
+
+  if (!options.skipSmokeTest) {
+    execWithLog(`yarn run lint:js && yarn run test`, debug.enabled);
+    console.log(`✅ ` + chalk.cyan(`Project passes Smoke Test`));
+  } else {
+    console.log(`⚠️ ` + chalk.grey(`Skipping Smoke Test`));
+  }
+
+  let nextVersion = options.currentVersion;
+  if (!options.skipVersion) {
+    // https://github.com/lerna/lerna/tree/master/commands/version#--exact
+    // We use exact to ensure that our consumers always use the appropriate
+    // versions published with each other
+    // --force-publish ensures that all packages release a new version regardless
+    // of whether changes have occurred in them
+    // --yes skips the prompt for confirming the version
+    if (options.useVersion) {
+      if (semver.valid(options.useVersion)) {
+        nextVersion = options.useVersion;
+      } else {
+        throw Error(`Version "${options.useVersion}" is not a valid semantic version.`);
+      }
+    } else {
+      nextVersion = retrieveNextVersion(options);
+    }
+    execWithLog(`lerna version ${nextVersion} --force-publish --exact --yes`, true);
+    console.log(`✅ ` + chalk.cyan(`Successfully Versioned ${nextVersion}`));
+  } else {
+    console.log('⚠️ ' + chalk.grey(`Skipping Versioning`));
+  }
+
+  if (!options.skipPack) {
+    cleanProject();
+    packAllPackages();
+    console.log(`✅ ` + chalk.cyan(`Successfully Packaged ${nextVersion}`));
+  } else {
+    console.log('⚠️ ' + chalk.grey(`Skipping Packaging`));
+  }
+
+  if (!options.skipPublish) {
+    const tarballs = collectTarballPaths();
+    const npmAuthTokenInEnv = !!process.env.NODE_AUTH_TOKEN;
+    if (!npmAuthTokenInEnv) {
+      console.log('No NODE_AUTH_TOKEN environment variable. Prompting for OTP.');
+
+      if (process.env.CI) {
+        throw new Error('No NODE_AUTH_TOKEN environment variable, cannot continue publishing.');
+      }
+    }
+    // Assume human ran script if token is missing
+    await confirmPublish(tarballs, options, !npmAuthTokenInEnv);
+    console.log(`✅ ` + chalk.cyan(`Successfully Published ${nextVersion}`));
+  } else {
+    console.log('⚠️ ' + chalk.grey(`Skipping Publishing`));
+  }
+}
+
+let cli = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+main()
+  .finally(() => cli.close())
+  .catch(reason => {
+    console.error(reason);
+    process.exit(1);
+  });
